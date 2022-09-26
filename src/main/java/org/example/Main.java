e5b66b2c12028852521d10fae024de8ec7fe1966
Marc Matter <marc@recapp.ch>
2022-09-26 17:37:58 +0200

    setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+
+        Employee daniel = new Employee();
+        daniel.setId(101);
+        daniel.setFirstName("Gaurav");
+        daniel.setLastName("Chawla");
+
+        session.save(daniel);
+        t.commit();
+        System.out.println("successfully saved");
+        factory.close();
+        session.close();
     }
 }
